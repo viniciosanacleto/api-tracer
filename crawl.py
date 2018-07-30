982a25afe40e9772b216cba66bd926d22015226b
Vinicios Anacleto <viniciosanacleto@hotmail.com>
2018-07-30 15:25:17 -0300

    Basic for GET endpoints and new wordlists added

--- crawl.py
+++ crawl.py
@@ -4,7 +4,7 @@ import requests
 args = sys.argv
 
 if len(args)<3:
-	sys.exit("Missing parameters: python3 crawl.py [API URL] [WORDLIST]")
+	sys.exit("Use: python3 crawl.py [http://API_URL] [WORDLIST]")
 
 url = args[1]
 wlfile = args[2]
@@ -12,6 +12,12 @@ wlfile = args[2]
 i = 0
 with open(wlfile,'r') as file:
 	readFile = file.read().splitlines()
+	print('This could take a while...')
 	for word in readFile:
-		r = requests.get(url+'/'+word)
-		print(r.status_code)
+		i += 1
+		requestUrl = url+'/'+word
+		r = requests.get(requestUrl)
+		if r.status_code == 200 or r.status_code == 401:
+			print('Gotcha: '+requestUrl)
+		if i%1000 == 0:
+			print(i)
