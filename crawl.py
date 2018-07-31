61cf7df7efae96c25df21288fb3b65914f043bbf
Vinicios Anacleto <viniciosanacleto@hotmail.com>
2018-07-31 17:15:29 -0300

    Functions separation

--- crawl.py
+++ crawl.py
@@ -1,5 +1,7 @@
 import sys
 import requests
+import threading
+import math
 
 args = sys.argv
 
@@ -8,16 +10,45 @@ if len(args)<3:
 
 url = args[1]
 wlfile = args[2]
+wordlist = []
+chunks = []
+nthreads = 3
 
-i = 0
-with open(wlfile,'r') as file:
-	readFile = file.read().splitlines()
-	print('This could take a while...')
-	for word in readFile:
+def doRequest(requestUrl):
+	r = requests.get(requestUrl)
+	if r.status_code == 200 or r.status_code == 401:
+		print('Gotcha: '+requestUrl)
+
+def readFile():
+	with open(wlfile,'r') as file:
+		readFile = file.read().splitlines()
+		for word in readFile:
+			wordlist.append(word)
+
+def splitWordList():
+	chunk_size = len(wordlist) / nthreads
+	l = []
+	i = 1
+	for word in wordlist:
+		l.append(word)
 		i += 1
-		requestUrl = url+'/'+word
-		r = requests.get(requestUrl)
-		if r.status_code == 200 or r.status_code == 401:
-			print('Gotcha: '+requestUrl)
-		if i%1000 == 0:
-			print(i)
+		if i == chunk_size:
+			chunks.append(l)
+			l = []
+			i = 0
+		
+def main():
+	i = 0
+	readFile()
+	splitWordList()
+	print(chunks)
+	if(len(wordlist)>0):
+		for word in wordlist:
+			doRequest(url+'/'+word)
+			i += 1
+			if i%1000 == 0:
+				print(i)
+	else:
+		print('No words in wordlist!')
+
+main()
