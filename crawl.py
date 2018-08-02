48ec28db7ccc7275ab85f8451fa239825aa2745f
Vinicios Anacleto <viniciosanacleto@hotmail.com>
2018-08-02 16:51:16 -0300

    Paralell requests

--- crawl.py
+++ crawl.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import threading
 import math
+from multiprocessing import Pool
 
 args = sys.argv
 
@@ -11,11 +12,10 @@ if len(args)<3:
 url = args[1]
 wlfile = args[2]
 wordlist = []
-chunks = []
-nthreads = 3
+nthreads = 50
 
 def doRequest(requestUrl):
-	r = requests.get(requestUrl)
+	r = requests.get(requestUrl)	
 	if r.status_code == 200 or r.status_code == 401:
 		print('Gotcha: '+requestUrl)
 
@@ -23,31 +23,15 @@ def readFile():
 	with open(wlfile,'r') as file:
 		readFile = file.read().splitlines()
 		for word in readFile:
-			wordlist.append(word)
+			wordlist.append(url+'/'+word)
 
-def splitWordList():
-	chunk_size = len(wordlist) / nthreads
-	l = []
-	i = 1
-	for word in wordlist:
-		l.append(word)
-		i += 1
-		if i == chunk_size:
-			chunks.append(l)
-			l = []
-			i = 0
-		
+
 def main():
-	i = 0
 	readFile()
-	splitWordList()
-	print(chunks)
 	if(len(wordlist)>0):
-		for word in wordlist:
-			doRequest(url+'/'+word)
-			i += 1
-			if i%1000 == 0:
-				print(i)
+		print('This could take a while...')
+		with Pool(nthreads) as p:
+			p.map(doRequest,wordlist)	
 	else:
 		print('No words in wordlist!')
 
